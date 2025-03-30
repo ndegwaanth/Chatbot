3b0f5599600e2c4f5bbbd3f5fea6be8d782df0e1
Anthony <ndegwaanthony300@gmail.com>
2025-03-31 02:11:05 +0300

    template setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Generative AI model",
+    version = "0.0.0",
+    author = 'Anthony Ndegwa',
+    author_email= "ndegwaanthony300@gmail.com",
+    packages=find_packages(),
+    install_requires= []
+)
